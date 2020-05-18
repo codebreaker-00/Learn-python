49213efe2227ff46f2f01fa3a766e4fb69e60cb2
Vishal SIngh <vishal.singh@students.iiit.ac.in>
2020-05-18 11:14:59 +0530

    Commit no - 4
    This has info about operations in Python

---
+++ operations_in_python.py
@@ -0,0 +1,34 @@
+# addition,substraction,multiplication and division is represented normal
+print(3+5)
+print(3-5)
+print(3*5)
+print(3/5) #dividing two numbers give you a float output
+
+#NOTE- there is no need to use endl or '\n' after print statement
+# exponentiation is representation as ' ** ' a**b is {a to the power b}
+print(3**5)
+
+#Modulo represented as
+print(5%3)
+
+# Increment can be represented as
+h = 2
+h = h+1
+h += 1
+
+# BODMAS rule is followed here
+print( 3 + 4*5 )
+print (3 + 4/5)
+
+# Anything with decimals is called a float and be aware of the floating point error
+print(0.2 + 0.1) #note the error once you print it
+
+# you can use underscore while writting a long number
+num = 14_000000_000
+print(num) #outputs 14000000000
+
+# multiple assignments like this
+x,y,z = 0,1,2
+
+# CAPS_VARIABLE (written in upper case) is a constant type variable
+MY_FISRT_CONSTANT = 123456789
