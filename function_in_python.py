c832380cf60b6a69a5321a082ed9821e6665d300
Vishal Singh <vishal.singh@students.iiit.ac.in>
2020-05-21 18:54:17 +0530

    Functions in py

---
+++ function_in_python.py
@@ -0,0 +1,20 @@
+def greet():
+    print("Hello")
+    print("Good Morning")
+
+greet()
+
+def lister(x):
+    x[0]=20
+
+lis = [1,2,3,5,4]
+
+lister(lis)
+print(lis)
+
+def incement(x):
+    x = x+1
+
+num = 2
+incement(num)
+print(num)    
